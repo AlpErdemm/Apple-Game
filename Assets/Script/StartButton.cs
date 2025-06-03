c71814a15a8cea063e3777d2a005cb8b4d2a89b0
Alp Erdem <mamikalp61@gmail.com>
2025-06-03 22:29:44 +0300

    Rearrange project files

--- Assets/Script/StartButton.cs
+++ Assets/Script/StartButton.cs
@@ -6,6 +6,11 @@ public class StartButton : MonoBehaviour
     public GridAppleSpawner spawner;
     private void OnTriggerEnter(Collider other)
     {
+        OnStart();
+    }
+
+    public void OnStart()
+    {
         spawner.OnStartButton();
         gameObject.SetActive(false);
     }
